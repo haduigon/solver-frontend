67e5a12974bbafd721eedb6c2cfc24617e9b2cc3
haduigon <haduigon@gmail.com>
2024-08-26 15:33:39 +0300

    fb style butto is done

--- src/components/SocialNetworkLogin/SocialNetworkLogin.tsx
+++ src/components/SocialNetworkLogin/SocialNetworkLogin.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable */
 import styles from './SocialNetworkLogin.module.scss';
 import classNames from 'classnames';
 import * as userActions from '../../features/user';
 import { useAppDispatch } from '../../app/hooks';
 // import FacebookLogin from 'react-facebook-login';
-// import facebookPicture from '../../assets/img/fb.svg';
+import facebookPicture from '../../assets/img/fb.svg';
+// import TiSocialFacebookCircular from 'react-icons/lib/ti/social-facebook-circular';
 
-// import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+// import { useEffect } from 'react';
+// import FB from 'fb';
+
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
 type Props = {
   image: string,
@@ -20,6 +25,7 @@ type Props = {
 //   )
 // }
 
+
 const SocialNetworkLogin: React.FC<Props> = ({ image, name }) => {
   const color = styles.back;
   const dispatch = useAppDispatch();
@@ -28,23 +34,31 @@ const SocialNetworkLogin: React.FC<Props> = ({ image, name }) => {
     dispatch(userActions.userGoogleLogin());
   }
 
-  // const handleFbCall = (response: any) => {
-  //   console.log(response, 'fbcall response');
-  // }
+  const handleFbCall = (response: any) => {
+    console.log(response, 'fbcall response');
+  }
 
-  // if (name === 'facebook') {
-  //   return (
-  //     <FacebookLogin
-  //       // buttonStyle={{ padding: "6px" }}
-  //       appId="1253536009352522"  // we need to get this from facebook developer console by setting the app.
-  //       autoLoad={false}
-  //       fields="name,email,picture"
-  //       callback={handleFbCall}
-  //       // render={<image src={} />}
-  //       // icon={<>{facebookPicture}</>}
-  //     />
-  //   );
-  // }
+  if (name === 'facebook') {
+    return (
+      <FacebookLogin
+        // buttonStyle={{ padding: "6px" }}
+        appId="1253536009352522"  // we need to get this from facebook developer console by setting the app.
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={handleFbCall}
+        render={renderProps => (
+          <div onClick={renderProps.onClick} >
+            <img src={facebookPicture} alt="Facebook icon" />
+            
+          </div>
+        )}
+        // cssClass={styles.icon} // Apply custom styles using this prop
+// 
+        // icon={<img src={facebookPicture} alt="Facebook icon" className={styles.icon} />}
+        // redirectUri='/chat'
+      />
+    );
+  }
 
   return (
     <div
